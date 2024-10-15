99d25a8fedbbe4a9e1e4fee5af5207f3629a73fd
GLEKOV <glekovofficial@gmail.com>
2024-10-15 14:18:55 +0300

    Created engine and sessionmaker inside class DatabaseHelper in db_helper.py + update in config settings  2.0

--- app/core/models/db_helper.py
+++ app/core/models/db_helper.py
@@ -1,4 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.config import settings
+
 
 class DatabaseHelper:
     def __init__(
@@ -8,7 +13,7 @@ class DatabaseHelper:
         echo_pool: bool = False,
         pool_size: int = 5,
         max_overflow: int = 10,
-    ):
+    ) -> None:
         self.engine = create_async_engine(
             url=url,
             echo=echo,
@@ -22,5 +27,18 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
-    async def dispose(self):
+    async def dispose(self) -> None:
         await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
+)
